c66a09e099320b3733ac2a2ed614d06da59b8037
akashsandhuo-cmyk <akashsandhuo@fccollege.edu.pk>
2026-05-17 09:46:48 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -12,6 +12,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: '/',
       },
     ],
-    sitemap: 'https://www.rizenergy.com/sitemap.xml',
+    sitemap: 'https://www.rizenergy.com/sitemap.xml', 
   }
 }
